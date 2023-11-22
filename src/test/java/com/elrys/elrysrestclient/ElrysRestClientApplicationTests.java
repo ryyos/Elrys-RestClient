c179a7bac0f1303b66a5479fb1e4d89080ace701
ryosyntaxx <riodwi12174@gmail.com>
2023-11-22 11:28:37 +0700

    first commit

---
+++ src/test/java/com/elrys/elrysrestclient/ElrysRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elrys.elrysrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElrysRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
